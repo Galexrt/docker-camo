993593623902cdabc180ba47d1e368d1bdf58016
Alexander Trost <Galexrt@googlemail.com>
2015-10-07 00:23:10 +0200

    Fix the git install directoy Should work now

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,11 @@
 
 export PATH="node-v4.1.2-linux-x64/bin:$PATH"
 
-if [ ! -d /home/camo/.git ]; then
-    cd /home/camo
-    git clone --recursive https://github.com/atmos/camo /home/camo
+if [ ! -d /home/camo/camo/.git ]; then
+    mkdir -p /home/camo/camo
+    git clone --recursive https://github.com/atmos/camo /home/camo/camo
 fi
-
 chown -R camo:camo /home/camo
-
 if [ -z "$CAMO_KEY" ]; then
   echo "No Camo Key given."
   exit 1
@@ -17,4 +15,4 @@ fi
 echo "NodeJs version"
 node -v
 echo "==="
-su camo -c "node /home/camo/server.js"
+su camo -c "node /home/camo/camo/server.js"
