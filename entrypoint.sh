6840e5f3d2234204caaa8dfd85d08d309a29a773
Alexander Trost <Galexrt@googlemail.com>
2015-10-08 16:39:22 +0200

    Remove the return 1 after no CAMO_KEY given

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ export PATH="node-v4.1.2-linux-x64/bin:$PATH"
 
 if [ ! -d /home/camo/camo/.git ]; then
     mkdir -p /home/camo/camo
+    rm -rf /home/camo/camo
     git clone --recursive https://github.com/atmos/camo /home/camo/camo
 fi
 chown -R camo:camo /home/camo
 if [ -z "$CAMO_KEY" ]; then
   echo "No Camo Key given."
-  exit 1
 fi
 
 echo "NodeJs version"
